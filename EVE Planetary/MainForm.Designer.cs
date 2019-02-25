8b0894aea160cc502e4c82be85965aca92468cb2
Oleg <pop457045@mail.ru>
2019-02-25 17:56:11 +0300

    1-st

--- EVE Planetary/MainForm.Designer.cs
+++ EVE Planetary/MainForm.Designer.cs
@@ -29,29 +29,26 @@
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.ID_clmn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Name_clmn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Buy_min_clmn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Buy_median_clmn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Buy_avg_clmn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Buy_max_clmn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Buy_self_clmn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabPrice = new System.Windows.Forms.TabPage();
             this.splitPrices = new System.Windows.Forms.SplitContainer();
+            this.DGVPrices = new System.Windows.Forms.DataGridView();
+            this.tabWorks = new System.Windows.Forms.TabPage();
+            this.tabCalc = new System.Windows.Forms.TabPage();
+            this.UpdatePrices = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
-            this.tabPage1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabPrice.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitPrices)).BeginInit();
             this.splitPrices.Panel1.SuspendLayout();
+            this.splitPrices.Panel2.SuspendLayout();
             this.splitPrices.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGVPrices)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPrice);
+            this.tabControl1.Controls.Add(this.tabWorks);
+            this.tabControl1.Controls.Add(this.tabCalc);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
@@ -59,78 +56,16 @@
             this.tabControl1.Size = new System.Drawing.Size(800, 450);
             this.tabControl1.TabIndex = 0;
             // 
-            // tabPage1
+            // tabPrice
             // 
-            this.tabPage1.Controls.Add(this.splitPrices);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(792, 424);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Prices";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(792, 424);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Calculations";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.ID_clmn,
-            this.Name_clmn,
-            this.Buy_min_clmn,
-            this.Buy_median_clmn,
-            this.Buy_avg_clmn,
-            this.Buy_max_clmn,
-            this.Buy_self_clmn});
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(786, 377);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // ID_clmn
-            // 
-            this.ID_clmn.HeaderText = "ID";
-            this.ID_clmn.Name = "ID_clmn";
-            // 
-            // Name_clmn
-            // 
-            this.Name_clmn.HeaderText = "Name";
-            this.Name_clmn.Name = "Name_clmn";
-            // 
-            // Buy_min_clmn
-            // 
-            this.Buy_min_clmn.HeaderText = "Buy(min)";
-            this.Buy_min_clmn.Name = "Buy_min_clmn";
-            // 
-            // Buy_median_clmn
-            // 
-            this.Buy_median_clmn.HeaderText = "Buy(median)";
-            this.Buy_median_clmn.Name = "Buy_median_clmn";
-            // 
-            // Buy_avg_clmn
-            // 
-            this.Buy_avg_clmn.HeaderText = "Buy(avg)";
-            this.Buy_avg_clmn.Name = "Buy_avg_clmn";
-            // 
-            // Buy_max_clmn
-            // 
-            this.Buy_max_clmn.HeaderText = "Buy(max)";
-            this.Buy_max_clmn.Name = "Buy_max_clmn";
-            // 
-            // Buy_self_clmn
-            // 
-            this.Buy_self_clmn.HeaderText = "Buy(self)";
-            this.Buy_self_clmn.Name = "Buy_self_clmn";
+            this.tabPrice.Controls.Add(this.splitPrices);
+            this.tabPrice.Location = new System.Drawing.Point(4, 22);
+            this.tabPrice.Name = "tabPrice";
+            this.tabPrice.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPrice.Size = new System.Drawing.Size(792, 424);
+            this.tabPrice.TabIndex = 0;
+            this.tabPrice.Text = "Prices";
+            this.tabPrice.UseVisualStyleBackColor = true;
             // 
             // splitPrices
             // 
@@ -142,10 +77,54 @@
             // 
             // splitPrices.Panel1
             // 
-            this.splitPrices.Panel1.Controls.Add(this.dataGridView1);
+            this.splitPrices.Panel1.Controls.Add(this.DGVPrices);
+            // 
+            // splitPrices.Panel2
+            // 
+            this.splitPrices.Panel2.Controls.Add(this.UpdatePrices);
             this.splitPrices.Size = new System.Drawing.Size(786, 418);
             this.splitPrices.SplitterDistance = 377;
             this.splitPrices.TabIndex = 1;
+            // 
+            // DGVPrices
+            // 
+            this.DGVPrices.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.DGVPrices.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGVPrices.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DGVPrices.Location = new System.Drawing.Point(0, 0);
+            this.DGVPrices.Name = "DGVPrices";
+            this.DGVPrices.Size = new System.Drawing.Size(786, 377);
+            this.DGVPrices.TabIndex = 0;
+            // 
+            // tabWorks
+            // 
+            this.tabWorks.Location = new System.Drawing.Point(4, 22);
+            this.tabWorks.Name = "tabWorks";
+            this.tabWorks.Padding = new System.Windows.Forms.Padding(3);
+            this.tabWorks.Size = new System.Drawing.Size(792, 424);
+            this.tabWorks.TabIndex = 2;
+            this.tabWorks.Text = "Formulas";
+            this.tabWorks.UseVisualStyleBackColor = true;
+            // 
+            // tabCalc
+            // 
+            this.tabCalc.Location = new System.Drawing.Point(4, 22);
+            this.tabCalc.Name = "tabCalc";
+            this.tabCalc.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCalc.Size = new System.Drawing.Size(792, 424);
+            this.tabCalc.TabIndex = 1;
+            this.tabCalc.Text = "Calculations";
+            this.tabCalc.UseVisualStyleBackColor = true;
+            // 
+            // UpdatePrices
+            // 
+            this.UpdatePrices.Location = new System.Drawing.Point(6, 4);
+            this.UpdatePrices.Name = "UpdatePrices";
+            this.UpdatePrices.Size = new System.Drawing.Size(88, 30);
+            this.UpdatePrices.TabIndex = 0;
+            this.UpdatePrices.Text = "UpdatePrices";
+            this.UpdatePrices.UseVisualStyleBackColor = true;
+            this.UpdatePrices.Click += new System.EventHandler(this.UpdatePrices_Click);
             // 
             // MainForm
             // 
@@ -154,13 +133,14 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tabControl1);
             this.Name = "MainForm";
-            this.Text = "EvE Planetary";
+            this.Text = "EvE Planetary Calculator";
             this.tabControl1.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabPrice.ResumeLayout(false);
             this.splitPrices.Panel1.ResumeLayout(false);
+            this.splitPrices.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitPrices)).EndInit();
             this.splitPrices.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DGVPrices)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -168,17 +148,12 @@
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPrice;
+        private System.Windows.Forms.DataGridView DGVPrices;
+        private System.Windows.Forms.TabPage tabCalc;
         private System.Windows.Forms.SplitContainer splitPrices;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ID_clmn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Name_clmn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Buy_min_clmn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Buy_median_clmn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Buy_avg_clmn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Buy_max_clmn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Buy_self_clmn;
+        private System.Windows.Forms.TabPage tabWorks;
+        private System.Windows.Forms.Button UpdatePrices;
     }
 }
 
--- EVE Planetary/MainForm.cs
+++ EVE Planetary/MainForm.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace EVE_Planetary
 {
@@ -15,6 +17,173 @@ namespace EVE_Planetary
         public MainForm()
         {
             InitializeComponent();
+            DGVPrices.DataSource = ReadXml();
+        }
+
+        private DataTable CreateTable()
+        {
+            DataTable dt = new DataTable("Matrials");
+            DataColumn colID = new DataColumn("ID", typeof(Int32));
+            DataColumn colName = new DataColumn("Name", typeof(String));
+
+            DataColumn colBuyMin = new DataColumn("Buy(min)", typeof(String));
+            DataColumn colBuyMedian = new DataColumn("Buy(median)", typeof(String));
+            DataColumn colBuyAvg = new DataColumn("Buy(avg)", typeof(String));
+            DataColumn colBuyMax = new DataColumn("Buy(max)", typeof(String));
+            DataColumn colBuySelf = new DataColumn("Buy(self)", typeof(String));
+
+            DataColumn colSellMin = new DataColumn("Sell(min)", typeof(String));
+            DataColumn colSellMedian = new DataColumn("Sell(median)", typeof(String));
+            DataColumn colSellAvg = new DataColumn("Sell(avg)", typeof(String));
+            DataColumn colSellMax = new DataColumn("Sell(max)", typeof(String));
+            DataColumn colSellSelf = new DataColumn("Sell(self)", typeof(String));
+
+            dt.Columns.Add(colID);
+            dt.Columns.Add(colName);
+
+            dt.Columns.Add(colBuyMin);
+            dt.Columns.Add(colBuyMedian);
+            dt.Columns.Add(colBuyAvg);
+            dt.Columns.Add(colBuyMax);
+            dt.Columns.Add(colBuySelf);
+
+            dt.Columns.Add(colSellMin);
+            dt.Columns.Add(colSellMedian);
+            dt.Columns.Add(colSellAvg);
+            dt.Columns.Add(colSellMax);
+            dt.Columns.Add(colSellSelf);
+
+            return dt;
+        }
+
+        private DataTable ReadXml()
+        {
+            DataTable dt = null;
+            try
+            {
+                //загружаем xml файл
+                XDocument xDoc = XDocument.Load(@"Resources/elements.xml");
+                //создаём таблицу
+                dt = CreateTable();
+                DataRow newRow = null;
+                //получаем все узлы в xml файле
+                foreach (XElement elm in xDoc.Descendants("material"))
+                {
+                    //создаём новую запись
+                    newRow = dt.NewRow();
+                    //проверяем наличие атрибутов (если требуется)
+                    if (elm.HasAttributes)
+                    {
+                        //проверяем наличие атрибута id
+                        if (elm.Attribute("id") != null)
+                        {
+                            //получаем значение атрибута
+                            newRow["ID"] = int.Parse(elm.Attribute("id").Value);
+                        }
+                    }
+                    #region проверяем наличие xml элементов
+                    if (elm.Element("name") != null)
+                    {
+                        newRow["Name"] = elm.Element("name").Value;
+                    }
+
+                    if (elm.Element("BuyMin") != null)
+                    {
+                        newRow["Buy(min)"] = elm.Element("BuyMin").Value;
+                    }
+                    if (elm.Element("BuyMed") != null)
+                    {
+                        newRow["Buy(median)"] = elm.Element("BuyMed").Value;
+                    }
+                    if (elm.Element("BuyAvg") != null)
+                    {
+                        newRow["Buy(avg)"] = elm.Element("BuyAvg").Value;
+                    }
+                    if (elm.Element("BuyMax") != null)
+                    {
+                        newRow["Buy(max)"] = elm.Element("BuyMax").Value;
+                    }
+                    if (elm.Element("BuySelf") != null)
+                    {
+                        newRow["Buy(self)"] = elm.Element("BuySelf").Value;
+                    }
+
+                    if (elm.Element("SellMin") != null)
+                    {
+                        newRow["Sell(min)"] = elm.Element("SellMin").Value;
+                    }
+                    if (elm.Element("SellMed") != null)
+                    {
+                        newRow["Sell(median)"] = elm.Element("SellMed").Value;
+                    }
+                    if (elm.Element("SellAvg") != null)
+                    {
+                        newRow["Sell(avg)"] = elm.Element("SellAvg").Value;
+                    }
+                    if (elm.Element("SellMax") != null)
+                    {
+                        newRow["Sell(max)"] = elm.Element("SellMax").Value;
+                    }
+                    if (elm.Element("SellSelf") != null)
+                    {
+                        newRow["Sell(self)"] = elm.Element("SellSelf").Value;
+                    }
+                    #endregion
+
+                    //добавляем новую запись в таблицу
+                    dt.Rows.Add(newRow);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return dt;
+        }
+
+
+        private void UpdatePrices_Click(object sender, EventArgs e)
+        {
+            for (int row = 0; row < DGVPrices.Rows.Count; row++)
+            {
+                if (DGVPrices.Rows[row].Cells["ID"].Value != null)
+                {
+                    XmlDocument doc1 = new XmlDocument();
+                    doc1.Load("https://api.evemarketer.com/ec/marketstat?typeid=" + DGVPrices.Rows[row].Cells["ID"].Value.ToString() + "&regionlimit=10000002");
+                    XmlElement root = doc1.DocumentElement;
+                    XmlNodeList Bnodes = root.SelectNodes("/exec_api/marketstat/type/buy");
+                    XmlNodeList Snodes = root.SelectNodes("/exec_api/marketstat/type/sell");
+
+                    foreach (XmlNode node in Bnodes)
+                    {
+                        string Bmin = node["min"].InnerText;
+                        string Bmax = node["max"].InnerText;
+                        string Bavg = node["avg"].InnerText;
+                        string Bmed = node["median"].InnerText;
+
+
+                        DGVPrices.Rows[row].Cells["Buy(min)"].Value = Bmin;
+                        DGVPrices.Rows[row].Cells["Buy(max)"].Value = Bmax;
+                        DGVPrices.Rows[row].Cells["Buy(avg)"].Value = Bavg;
+                        DGVPrices.Rows[row].Cells["Buy(median)"].Value = Bmed;
+                    }
+
+                    foreach (XmlNode node in Snodes)
+                    {
+                        string Smin = node["min"].InnerText;
+                        string Smax = node["max"].InnerText;
+                        string Savg = node["avg"].InnerText;
+                        string Smed = node["median"].InnerText;
+
+
+                        DGVPrices.Rows[row].Cells["Sell(min)"].Value = Smin;
+                        DGVPrices.Rows[row].Cells["Sell(max)"].Value = Smax;
+                        DGVPrices.Rows[row].Cells["Sell(avg)"].Value = Savg;
+                        DGVPrices.Rows[row].Cells["Sell(median)"].Value = Smed;
+                    }
+
+                }
+            }
         }
     }
 }
